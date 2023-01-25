25518354eac7185e5008139e3639c6fc8eb4c84f
marsh <marshmallow641@gmail.com>
2023-01-25 00:17:36 -0600

    Committing project with a few edits and a venv.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Works on Mac, not on Windows 10 - webdriver_manager can only be found/installed on Mac
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -6,8 +7,10 @@ import time
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
+url = "https://www.selenium.dev/selenium/web/web-form.html"
+
 if __name__ == '__main__':
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    driver.get(url)
     time.sleep(10.0)
 
     driver.quit()
